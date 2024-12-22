5de06c486e91154ebb087e3b41f9baf1d5ecc634
faissal <yahyakun87@gmail.com>
2024-12-22 17:04:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,163 @@
+const toggleBtn = document.getElementById('menu-icon');
+const navbar = document.getElementById('navbar');
+
+toggleBtn.addEventListener('click', function() {
+  if (navbar.style.display === 'none' || navbar.style.display === '') {
+    navbar.style.display = 'block';
+    toggleBtn.textContent = ''; // Change button text
+  } else {
+    navbar.style.display = 'none';
+    toggleBtn.textContent = ''; // Change button text
+  }
+});
+
+
+
+
+
+
+class Translate{
+
+         constructor(){
+
+          document.getElementById("arabic").addEventListener("click",()=>{
+
+            this.Translate("arabic")
+
+          });
+
+
+
+          this.Translate();
+
+   
+}
+
+Translate(language){
+
+
+  if(language == "arabic"){
+
+    document.getElementById("text-animation-ar").innerHTML = "أنا مطور ويب متخصص في تصميم مواقع الويب، وتقديم تجارب مستخدم استثنائية وتصميمات حديثة وعالية الجودة تلبي توقعات العملاء. مع عامين من الخبرة، أتقن HTML وCSS وJavaScript وأطر عمل مثل React. أنا ملتزم بتعزيز مهاراتي وضمان جودة موقع الويب لتحقيق رضا العملاء.";
+
+  }
+
+
+  if(language == "arabic"){
+
+    document.getElementById("hi ar").innerHTML = "مرحبا انا فيصل";
+
+  }
+
+
+  if(language == "arabic"){
+
+    document.getElementById("btn ar").innerHTML = "يؤجر";
+
+  }
+
+  if(language == "arabic"){
+
+    document.getElementById("btn ar2").innerHTML = "تواصل";
+
+  }
+
+  if(language == "arabic"){
+
+    document.getElementById("heading ar").innerHTML = "رحلتي";
+
+  }
+
+  
+
+  if(language == "arabic"){
+
+    document.getElementById("my journey-p ar1").innerHTML = "ستتساءل لماذا أبحث عن حل والحل هو لماذا؟ لم أنجح في دراستي وفي مجتمعنا هذه دراسة مهمة للعمل لذلك هنا نسأل لماذا اخترت البرمجة؟";
+
+  }
+
+
+  if(language == "arabic"){
+
+    document.getElementById("my journey-p ar2").innerHTML = "كما هو معلوم فإن برمجة المواقع الإلكترونية مطلوبة بشكل كبير في المغرب، وهذا المجال لا يتطلب في أغلب الأحيان شهادة جامعية، ومن هنا بدأ الحلم... بدأت بالبحث وفهم هذا المجال، وأحببته، لكن الظروف لم تساعدني في ذلك الوقت";
+
+  }
+
+  if(language == "arabic"){
+
+    document.getElementById("my journey-p ar3").innerHTML = "كان الحظ بجانبي عندما كنت أملك جهاز كمبيوتر حتى لو كان ضعيفا، كنت أرى فيه مفتاحا لشغفي، فبدأت أذهب إلى المقاهي والأماكن التي يتوفر فيها إنترنت مجاني فقط لتحميل الدورات على الهاتف والعودة إلى المنزل ومشاهدتها وتطبيقها، كانت هذه بدايتي في برمجة العديد من المشاريع التي أحببتها وأعطتني الرغبة في إكمالها حتى لو لم تساعدني الإمكانيات";
+
+  }
+
+  if(language == "arabic"){
+
+    document.getElementById("my journey-p ar4").innerHTML = "بدأت في إنشاء مشاريع جيدة وأتعلم أكثر فأكثر كل يوم، تعلمت كيفية إنشاء موقع من الصفر، وعرضت بعض أعمالي على الناس وحصلت على دعم كبير، والحمد لله هذا حفزني أكثر، ورغم أنني لم أكسب دولاراً واحداً إلا أن هذا الحلم الجميل سيبقى أمامي حتى أحققه";
+
+  }
+
+  if(language == "arabic"){
+
+    document.getElementById("my journey-p ar5").innerHTML = "لم يتبقى سوى أيام معدودة حتى حلول العام الجديد، وقد وعدت نفسي ألا أضيع ولو القليل من الوقت فيها، وأن أطور نفسي أكثر فأكثر ببساطة لأنني أحب عملي";
+
+  }
+
+
+
+  if(language == "arabic"){
+
+    document.getElementById("heading 2").innerHTML = "";
+
+  }
+
+
+
+
+
+
+
+
+  else if(language == "arabic"){
+
+
+
+
+      document.getElementById("hi ar").innerHTML = "مرحبا انا فيصل";
+
+
+
+      document.getElementById("text-animation-ar").innerHTML = "أنا مطور ويب متخصص في تصميم مواقع الويب، وتقديم تجارب مستخدم استثنائية وتصميمات حديثة وعالية الجودة تلبي توقعات العملاء. مع عامين من الخبرة، أتقن HTML وCSS وJavaScript وأطر عمل مثل React. أنا ملتزم بتعزيز مهاراتي وضمان جودة موقع الويب لتحقيق رضا العملاء.";
+
+    
+
+    
+      
+       document.getElementById("hi ar").innerHTML = "يؤجر";
+       document.getElementById("hi ar").innerHTML = "تواصل";
+
+
+
+       document.getElementById("heading ar").innerHTML = "رحلتي";
+
+
+       document.getElementById("my journey-p ar1").innerHTML = "ستتساءل لماذا أبحث عن حل والحل هو لماذا؟ لم أنجح في دراستي وفي مجتمعنا هذه دراسة مهمة للعمل لذلك هنا نسأل لماذا اخترت البرمجة؟";
+       document.getElementById("my journey-p ar2").innerHTML = "كما هو معلوم فإن برمجة المواقع الإلكترونية مطلوبة بشكل كبير في المغرب، وهذا المجال لا يتطلب في أغلب الأحيان شهادة جامعية، ومن هنا بدأ الحلم... بدأت بالبحث وفهم هذا المجال، وأحببته، لكن الظروف لم تساعدني في ذلك الوقت";
+       document.getElementById("my journey-p ar3").innerHTML = "كان الحظ بجانبي عندما كنت أملك جهاز كمبيوتر حتى لو كان ضعيفا، كنت أرى فيه مفتاحا لشغفي، فبدأت أذهب إلى المقاهي والأماكن التي يتوفر فيها إنترنت مجاني فقط لتحميل الدورات على الهاتف والعودة إلى المنزل ومشاهدتها وتطبيقها، كانت هذه بدايتي في برمجة العديد من المشاريع التي أحببتها وأعطتني الرغبة في إكمالها حتى لو لم تساعدني الإمكانيات";
+       document.getElementById("my journey-p ar4").innerHTML = "بدأت في إنشاء مشاريع جيدة وأتعلم أكثر فأكثر كل يوم، تعلمت كيفية إنشاء موقع من الصفر، وعرضت بعض أعمالي على الناس وحصلت على دعم كبير، والحمد لله هذا حفزني أكثر، ورغم أنني لم أكسب دولاراً واحداً إلا أن هذا الحلم الجميل سيبقى أمامي حتى أحققه";
+       document.getElementById("my journey-p ar5").innerHTML = "لم يتبقى سوى أيام معدودة حتى حلول العام الجديد، وقد وعدت نفسي ألا أضيع ولو القليل من الوقت فيها، وأن أطور نفسي أكثر فأكثر ببساطة لأنني أحب عملي";
+
+
+       document.getElementById("heading 2").innerHTML = "";
+
+
+  
+  }
+
+
+
+      }
+};
+
+onload = new Translate();
+
+
+
